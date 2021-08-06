8512245262be2eae6dcde79cbed1398bf3153ca5
chgl <chgl@users.noreply.github.com>
2021-08-06 13:56:49 +0200

    feat: added kubent and fixed pluto check

--- scripts/chart-powerlint.sh
+++ scripts/chart-powerlint.sh
@@ -4,7 +4,7 @@ set -euox pipefail
 CHARTS_DIR=${CHARTS_DIR:-"charts"}
 SHOULD_UPDATE_DEPENDENCIES=${SHOULD_UPDATE_DEPENDENCIES:-""}
 
-KUBERNETES_VERSIONS=${KUBERNETES_VERSIONS:-"1.18.0 1.19.0 1.20.0 1.21.0"}
+KUBERNETES_VERSIONS=${KUBERNETES_VERSIONS:-"1.18.0 1.19.0 1.20.0 1.21.0 1.22.0"}
 
 POLARIS_SCORE_THRESHOLD=${POLARIS_SCORE_THRESHOLD:-90}
 SKIP_KUBE_SCORE=${SKIP_KUBE_SCORE:-"1"}
@@ -34,10 +34,9 @@ for CHART_PATH in "${CHARTS_DIR}"/*; do
     echo "Helm lint..."
     helm lint "${CHART_PATH}"
 
-    echo "Kubeconform check..."
-
     for KUBERNETES_VERSION in ${KUBERNETES_VERSIONS}; do
         echo "Validating against Kubernetes version $KUBERNETES_VERSION:"
+        echo "Kubeconform check..."
 
         if ! helm template ${HELM_TEMPLATE_ARGS} ${CHART_PATH} |
             kubeconform \
@@ -49,6 +48,14 @@ for CHART_PATH in "${CHARTS_DIR}"/*; do
                 -exit-on-error \
                 -summary -; then
             echo "kubeconform validation failed"
+            exit 1
+        fi
+
+        echo "Pluto check..."
+
+        if ! helm template ${HELM_TEMPLATE_ARGS} ${CHART_PATH} |
+            pluto detect --target-versions k8s=v$KUBERNETES_VERSION -; then
+            echo "Pluto failed"
             exit 1
         fi
     done
@@ -69,13 +76,6 @@ for CHART_PATH in "${CHARTS_DIR}"/*; do
             --set-exit-code-on-danger \
             --set-exit-code-below-score $POLARIS_SCORE_THRESHOLD; then
         echo "Polaris failed"
-        exit 1
-    fi
-
-    echo "Pluto check..."
-
-    if ! helm template ${HELM_TEMPLATE_ARGS} ${CHART_PATH} | pluto detect -; then
-        echo "Pluto failed"
         exit 1
     fi
 
